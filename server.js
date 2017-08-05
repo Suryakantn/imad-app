bb591cc9980c9aef5d460479e98b011914cebe64
Suryakantn <30419592+Suryakantn@users.noreply.github.com>
2017-08-05 11:00:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,43 @@ var articleOne={
                             This is what i want ,this is content to my 1st article.... 
                       </p> `
 };
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate=`
+<html>
+ <head>
+         <title>
+              ${title}
+         </title>
+                        <meta name="viewport" content="width=device-width, initial-scale=1" />
+                        <link href="/ui/style.css" rel="stylesheet" />
+ </head> 
+      <body>
+          <div class="container">
+                   <div>
+                        <a href= "/">Home</a>
+                   
+                   </div>
+               <hr/>
+                   <h3>
+                        ${heading}
+                   </h3>
+               <div>
+                        ${date}
+               </div>
+               <div>
+                      ${content}
+                </div>
+        </div>
+      </body>
+</html>
+`; return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -30,7 +67,7 @@ app.get('/Article-one',function(req,res){
 });
 
 app.get('/Article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
+    res.send(createTemplate(articleOne))
 });
 app.get('/Article-three',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
