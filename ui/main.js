fd338948799efe6e2b0332fef8532a53bf976910
Suryakantn <30419592+Suryakantn@users.noreply.github.com>
2017-08-06 14:17:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ element.innerHTML='New Value';
 
 //Slide image new Left side
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
