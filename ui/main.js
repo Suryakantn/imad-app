f50aef3df80f18470bf43bcb4f49f3a6ef05eb1d
Suryakantn <30419592+Suryakantn@users.noreply.github.com>
2017-08-06 14:09:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //Change the value of class
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//Slide image new Left side
+var img=document.getElementById('madi');
+    img.onclick=function(){
+      img.style.marginLeft='100px';  
+    };
+    
